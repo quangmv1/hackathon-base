a8f5c0104d7d1adfcef627a7a4369d9b3a68612f
phuhs <phuhs@coin98.finance>
2025-01-18 22:59:46 +0700

    feat: add loading

--- packages/plugins/pluginPortfolio/src/components/TarotContainer/CardTarot.tsx
+++ packages/plugins/pluginPortfolio/src/components/TarotContainer/CardTarot.tsx
@@ -4,21 +4,27 @@ import { TarotCard } from '../../assets/data';
 import './style.css';
 
 type Props = {
-  isOpened?: boolean;
   data: TarotCard;
+  isOpened?: boolean;
+  isShowName?: boolean;
+  isLoading?: boolean;
 };
 
-function CardTarot({ isOpened, data }: Props) {
+function CardTarot({ isOpened, data, isShowName, isLoading }: Props) {
   return (
     <div
-      className={cn('flip-card aspect-[350/600] cursor-pointer ', {
+      className={cn('flip-card aspect-[350/600]', {
         'flip-card-opened': isOpened,
+        // 'animate-wiggle': isLoading,
+        'animate-heartBeat': isLoading,
+        // 'animate-bounce': isLoading,
       })}
     >
       <div className="flip-card-inner">
         <div className="flip-card-front">
           {/* <img src={data.img} alt="Avatar" /> */}
           <img
+            // src="/images/back2.jpg"
             src="/images/back-tarot.png"
             alt="bg-header"
             className="hidden md:block h-full absolute -z-[1] w-full object-contain"
@@ -32,6 +38,11 @@ function CardTarot({ isOpened, data }: Props) {
           />
         </div>
       </div>
+      {isShowName && (
+        <div className="text-center text-lg">
+          <strong className="text-sm">{data.name}</strong>
+        </div>
+      )}
     </div>
   );
 }
